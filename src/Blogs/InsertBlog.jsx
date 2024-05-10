fbb0db328d73f74bd8c6fa00eb41afed7affa9da
nickbelwin <belwalkarsarvesh@gmail.com>
2024-05-10 16:09:17 +0530

    update

--- src/Blogs/InsertBlog.jsx
+++ src/Blogs/InsertBlog.jsx
@@ -1,25 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from 'react';
 
-function InsertBlog(props) { 
-    const [formData,setFormData]=useState({
-        title:"",
-        description:""
+function InsertBlog(props) {
+    const [formData, setFormData] = useState({
+        title: "",
+        description: ""
     });
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
+    const [allData, setAllData] = useState([]);
 
-    const postBlog= async()=>{
+    const getBlogData = async () => {
         try {
             setLoading(true);
-            let res= await fetch("http://localhost:4000/insertBlog",{
-                method:"POST",
+            let res = await fetch("https://blog-backend-inky.vercel.app/getBlogs");
+            let resJson = await res.json();
+            console.log(resJson.data);
+            setAllData(resJson.data)
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        getBlogData();
+    }, [])
+
+    const postBlog = async () => {
+        try {
+            setLoading(true);
+            let res = await fetch("https://blog-backend-inky.vercel.app/insertBlog", {
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({...formData})
+                body: JSON.stringify({ ...formData })
             });
-            let resJson= await res.json();
+            let resJson = await res.json();
             console.log(resJson);
+            getBlogData();
             setLoading(false);
         } catch (error) {
             console.log(error);
@@ -30,10 +49,20 @@ function InsertBlog(props) {
 
     return (
         <div>
-            <input type="text" onChange={(e)=>{ setFormData({ ...formData, title:e.target.value })}} />
-            <textarea onChange={(e)=>{ setFormData({ ...formData, description:e.target.value })}}></textarea>
-            <button onClick={postBlog}>submit</button>
-            {loading? <h1>Loading...</h1>:null }
+            <input type="text" onChange={(e) => { setFormData({ ...formData, title: e.target.value }) }} />
+            <textarea onChange={(e) => { setFormData({ ...formData, description: e.target.value }) }}></textarea>
+            <button onClick={postBlog}>{loading ? "Loading..." : "Submit"}</button>
+            {!loading ?
+                allData?.map((val) => {
+                    return (
+                        <div>
+                            <h1 >{val?.title}</h1>
+                            <p>{val?.description}</p>
+                        </div>
+                    )
+                })
+                : null}
+
         </div>
     );
 }
