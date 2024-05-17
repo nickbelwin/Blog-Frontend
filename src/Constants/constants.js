6b3b5067964876d508d226835445b8edf3a1c1e5
nickbelwin <belwalkarsarvesh@gmail.com>
2024-05-17 23:59:28 +0530

    updated

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,5 +1,5 @@
 // export const basePath="http://localhost:4000"
-export const basePath = "https://blog-backend-inky.vercel.app"
+export const basePath = "https://blog-backend-zeta-seven.vercel.app"
 
 export const navList = [
     { path: "", name: "Posh" },
